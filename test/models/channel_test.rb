3ab0d3722e8ce4aee1dedee54bcae5f584834462
h3565144 <h3565144@gmail.com>
2014-10-26 01:05:02 +0800

    refine channel test

--- test/models/channel_test.rb
+++ test/models/channel_test.rb
@@ -3,12 +3,11 @@ require 'test_helper'
 class ChannelTest < ActiveSupport::TestCase
 
   test 'should fetch_items' do
-    # channel = create :channel, url: 'https://www.ruby-lang.org/en/feeds/news.rss'
-    channel = create :channel, url: 'http://news.163.com/special/00011K6L/rss_gj.xml'
+    channel = create :channel, url: 'https://www.ruby-lang.org/en/feeds/news.rss'
     channel.fetch_items
-    assert channel.items.count, 50
+    assert_equal channel.items.count, 10
     channel.fetch_items
-    assert channel.items.count, 50
+    assert_equal channel.items.count, 10
   end
 
 end
