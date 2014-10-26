5d2cf7558d9cd7fc3e40d78c8846ef6c2f1a9f93
h3565144 <h3565144@gmail.com>
2014-10-26 09:56:59 +0800

    refine ChannelsControllerTest

--- test/controllers/channels_controller_test.rb
+++ test/controllers/channels_controller_test.rb
@@ -1,47 +1,23 @@
 require 'test_helper'
 
 class ChannelsControllerTest < ActionController::TestCase
-  setup do
-    @channel = channels(:one)
-  end
-
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:channels)
-  end
-
-  test "should get new" do
+  test 'should get new' do
     get :new
     assert_response :success
   end
 
-  test "should create channel" do
+  test 'should create channel' do
     assert_difference('Channel.count') do
-      post :create, channel: { fetched_at: @channel.fetched_at, text: @channel.text, url: @channel.url }
+      post :create, channel: {url: 'https://www.ruby-lang.org/en/feeds/news.rss'}
     end
 
-    assert_redirected_to channel_path(assigns(:channel))
+    assert_redirected_to reader_path
   end
 
-  test "should show channel" do
-    get :show, id: @channel
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, id: @channel
-    assert_response :success
-  end
-
-  test "should update channel" do
-    patch :update, id: @channel, channel: { fetched_at: @channel.fetched_at, text: @channel.text, url: @channel.url }
-    assert_redirected_to channel_path(assigns(:channel))
-  end
-
-  test "should destroy channel" do
+  test 'should destroy channel' do
+    channel = create :channel, url: 'https://www.ruby-lang.org/en/feeds/news.rss' 
     assert_difference('Channel.count', -1) do
-      delete :destroy, id: @channel
+      delete :destroy, id: channel
     end
 
     assert_redirected_to channels_path
